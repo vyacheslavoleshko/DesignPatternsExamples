3c3e88ac73ae62f524e3e920e023ccdf8428f2b5
vyacheslavoleshko <vyacheslavoleshko@gmail.com>
2017-10-10 09:39:59 +0300

    Refactored COMPOSITE

--- COMPOSITE (tree-with-containers-with-containers...)/CodeExample/src/Main.java
+++ COMPOSITE (tree-with-containers-with-containers...)/CodeExample/src/Main.java
@@ -4,30 +4,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        Folder documents = new Folder("MY DOCUMENTS");
-        Folder music = new Folder("Music");
-        Folder movies = new Folder("Movies");
-        Folder workFiles = new Folder("Work Files");
-        Folder linkinPark = new Folder("Linkin Park");
+        // Generate FOLDERS
+        Data documents  = new Folder("MY DOCUMENTS");
+        Data music      = new Folder("Music");
+        Data movies     = new Folder("Movies");
+        Data workFiles  = new Folder("Work Files");
+        Data linkinPark = new Folder("Linkin Park");
 
-        File shawshankRedemption = new File("Shawshank Redemption.avi");
-        File workFile = new File("App.java");
-        File oneMoreLight = new File("One more light.mp3");
-        File leaveOutAllTheRest = new File("Leave out all the rest.mp3");
+        // Generate FILES
+        Data shawshankRedemption = new File("Shawshank Redemption.avi");
+        Data workFile            = new File("App.java");
+        Data oneMoreLight        = new File("One more light.mp3");
+        Data leaveOutAllTheRest  = new File("Leave out all the rest.mp3");
 
+        // Add FOLDERS to FOLDERS
         documents.add(workFiles);
         documents.add(movies);
         documents.add(music);
 
+        // Add FILES to FOLDERS
         workFiles.add(workFile);
-
         movies.add(shawshankRedemption);
-
         music.add(linkinPark);
-
         linkinPark.add(oneMoreLight);
         linkinPark.add(leaveOutAllTheRest);
 
+        // Display final TREE
         documents.display();
     }
 }
